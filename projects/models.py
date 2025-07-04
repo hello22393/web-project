37e96ac1aa9da3ea345a6d6c3492fba46d12ba76
서지현 <3223sni@dankook.ac.kr>
2025-07-04 23:07:15 +0900

    프로젝트 & 투표 모델 정의하고  마이그레이션까지

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def average_score(self):
+        scores = self.votes.all()
+        return sum(v.score for v in scores) / len(scores) if scores else 0
+
+    def __str__(self):
+        return self.title
+
+class Vote(models.Model):
+    project = models.ForeignKey(Project, related_name='votes', on_delete=models.CASCADE)
+    score = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    submitted_at = models.DateTimeField(auto_now_add=True)
