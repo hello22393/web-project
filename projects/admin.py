9ee738fc021d612966978cf681bdb7b1314a1a8e
서지현 <3223sni@dankook.ac.kr>
2025-07-04 23:11:16 +0900

    admin 페이지 구성

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project, Vote
 
-# Register your models here.
+class VoteInline(admin.TabularInline):
+    model = Vote
+    extra = 0
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'average_score']
+    inlines = [VoteInline]
+
+admin.site.register(Vote)
